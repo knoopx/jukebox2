1737794ca7092690ee6be1a8cb35166be65b4046
Víctor Martínez <knoopx@gmail.com>
2012-01-20 18:43:12 +0100

    Validate path existence

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,4 +1,8 @@
 class Source < ActiveRecord::Base
   validates_presence_of :path
   validates_uniqueness_of :path
+
+  validate do
+    errors.add :path, "does not exist" unless Dir.exist?(self.path)
+  end
 end
