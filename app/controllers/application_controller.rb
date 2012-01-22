7f5c711fb81d697ab7d6db8a9d5ba7ed2795b9ce
Víctor Martínez <knoopx@gmail.com>
2012-01-22 15:45:02 +0100

    Better way of handling pjax requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,19 @@ class ApplicationController < ActionController::Base
 
   include InheritedResources::ControllerScopes
 
-  before_filter :pjax
-
   include Jukebox2::Navigation
   include Jukebox2::Filtering::ControllerMethods
   include Jukebox2::Search::ControllerMethods
   include Jukebox2::Sorting::ControllerMethods
   include Jukebox2::Pagination::ControllerMethods
 
-  def pjax
+  def _default_layout_with_pjax(*args)
     if request.headers['X-PJAX']
-      render :layout => false
+      false
+    else
+      _default_layout_without_pjax(*args)
     end
   end
+
+  alias_method_chain :_default_layout, :pjax
 end
